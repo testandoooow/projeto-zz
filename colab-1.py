beabe32dfa9a023a71a58ad91d98e06c87277254
Kaique.s <141377576+testandoooow@users.noreply.github.com>
2024-10-31 12:54:34 +0000

    Solicitando endereço

--- colab-1.py
+++ colab-1.py
@@ -3,9 +3,14 @@ import os
 os.system("cls || clear")
 print("Kaique")
 
+# Solicitando dados.
+endereco = input("Digite seu endereço: ")
 # Comandos do git
 # git pull = Atualiza o repositório atual com dados do repositorio no GitHub.
 # git add . = Adiciona novos dados para o commit.
 # git commit -m "Título para o commit" = Versiona com os novos dados.
 # git pull = Atualiza trazendo commits do GitHub.
 # git push = Envia os commits para o GitHub
+# git rebase --continue = atualizar
+# git branch = visualizar as branch
+# git checkout -b = criar branch
