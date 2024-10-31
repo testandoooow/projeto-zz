099bf5b614397378f90bad5553cd51eb1e451e3b
Kaique.s <141377576+testandoooow@users.noreply.github.com>
2024-10-31 11:59:16 +0000

    agua

---
+++ colab-1.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("cls || clear")
+print("Kaique")
+
+# Comandos do git
+# git pull = Atualiza o repositório atual com dados do repositorio no GitHub.
+# git add . = Adiciona novos dados para o commit.
+# git commit -m "Título para o commit" = Versiona com os novos dados.
+# git pull = Atualiza trazendo commits do GitHub.
+# git push = Envia os commits para o GitHub
